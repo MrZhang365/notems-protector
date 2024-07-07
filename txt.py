da6dade941c7a4e8513f49f6a82db472235a0c3c
lightworld689 <107845122+lightworld689@users.noreply.github.com>
2024-07-07 13:39:50 +0800

    wssb fix bug

--- txt.py
+++ txt.py
@@ -1,11 +1,10 @@
-import os
 overlay_text = """
 protecting this page!
 --DQJL
 """
 
-print("利仔干森莫？这只是一个写文本的阿喂！")
-print("(#`O′)")
-print()
-print("按任意键退出")
-os.system(pause > nul)
+if __name__ == "__main__":
+    print("利仔干森莫？这只是一个写文本的阿喂！")
+    print("(#`O′)")
+    print()
+    input("按任意键退出")
